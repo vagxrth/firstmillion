36ae067b1f35ab324a6adb00fe255ef95d3ca67d
Vagarth <vagarth419@gmail.com>
2024-06-23 22:01:12 +0530

    feat: created landing page

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async (props: Props) => {
         <header className='fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between'>
             <aside className="flex items-center gap-[2px]">
                 <p className="text-3xl font-bold">
-                    Next SaaS
+                    First
                 </p>
                 <Image src="/logo.png"
                     width={15}
@@ -18,7 +18,7 @@ const Navbar = async (props: Props) => {
                     alt="logo"
                     className="shadow-sm" />
                 <p className="text-3xl font-bold">
-                    Idea
+                    Million
                 </p>
             </aside>
             <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
